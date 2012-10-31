1ab1d547df5b057fcc05ae69069ae284ede8c579
Matthew Lavin <matt5784@gmail.com>
2012-10-31 14:57:03 -0500

    Added a .gitignore file. Implemented a view which displays the camera output in real time (based on code from the android developer site). Edited the layout for the main activity. Updated the Manifest.

--- src/edu/vu/augmented/reality/MainActivity.java
+++ src/edu/vu/augmented/reality/MainActivity.java
@@ -1,20 +1,55 @@
 package edu.vu.augmented.reality;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
 
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview,0);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
