ddffdb36b1da152d1ed4965d9acacab5c7893ffa
Matthew Lavin <matt5784@gmail.com>
2012-11-01 20:45:09 -0500

    Changed compiler to 1.6, modified the main screen slightly for non-compatible devices.

--- src/edu/vu/augmented/reality/MainActivity.java
+++ src/edu/vu/augmented/reality/MainActivity.java
@@ -3,10 +3,14 @@ package edu.vu.augmented.reality;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Typeface;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -16,15 +20,35 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
-
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview,0);
+        if (checkCameraHardware(this)){
+	        setContentView(R.layout.activity_main);
+	        
+	        // Create an instance of Camera
+	        mCamera = getCameraInstance();
+	
+	        // Create our Preview view and set it as the content of our activity.
+	        mPreview = new CameraPreview(this, mCamera);
+	        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+	        preview.addView(mPreview,0);
+        } else {
+	        TextView errorView;
+	        errorView = new TextView(getApplicationContext());
+	        errorView.setBackgroundResource(android.R.drawable.dark_header);
+	        errorView.setGravity(Gravity.CENTER_VERTICAL);
+	        errorView.setFocusable(false);
+	        errorView.setClickable(false);//TODO figure out why the partition is clickable...
+	        errorView.setLongClickable(false);
+	        errorView.setOnClickListener(new View.OnClickListener() {
+	            
+	            @Override
+	            public void onClick(View v) {//Temporary so the app doesn't crash when partitions are clicked
+	            }
+	        });
+	        errorView.setTextSize((float) 14.0);
+	        errorView.setTypeface(Typeface.DEFAULT_BOLD);
+	        errorView.setText("Sorry, your device either does not have a camera or its camera is not compatible with this application.");
+	        setContentView(errorView);
+        }
     }
 
     @Override
