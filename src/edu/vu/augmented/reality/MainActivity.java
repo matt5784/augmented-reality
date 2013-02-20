7df4f6355834b2eb3738fc9a699072b22701a07b
Matthew Lavin <matt5784@gmail.com>
2013-02-20 16:12:17 -0600

    Changed some things around so the app can pause and resume properly.

--- src/edu/vu/augmented/reality/MainActivity.java
+++ src/edu/vu/augmented/reality/MainActivity.java
@@ -24,55 +24,38 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (checkCameraHardware(this)){
-	        setContentView(R.layout.activity_main);
-	        
-	        // Create an instance of Camera
-	        mCamera = getCameraInstance();
-	        Camera.Parameters cp = mCamera.getParameters();
-	        List<Camera.Size> sizes = cp.getSupportedPreviewSizes();
-	        /*for (int i = 0; i < sizes.size(); i++){
-		        Toast.makeText(getApplicationContext(), "height " + i + ": "+cp.getPreviewSize().height+"", Toast.LENGTH_SHORT).show();
-		        Toast.makeText(getApplicationContext(), "width " + i + ": " +cp.getPreviewSize().width+"", Toast.LENGTH_SHORT).show();
-	        }*/
-	        cp.setPreviewSize(sizes.get(sizes.size()-1).width, sizes.get(sizes.size()-1).height);
-	        mCamera.setParameters(cp);
-	        
-	        // Create our Preview view and set it as the content of our activity.
-	        mPreview = new CameraPreview(this, mCamera);
-	        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-	        preview.addView(mPreview,0);
-        } else {
-	        TextView errorView;
-	        errorView = new TextView(getApplicationContext());
-	        errorView.setBackgroundResource(android.R.drawable.dark_header);
-	        errorView.setGravity(Gravity.CENTER_VERTICAL);
-	        errorView.setFocusable(false);
-	        errorView.setClickable(false);//TODO figure out why the partition is clickable...
-	        errorView.setLongClickable(false);
-	        errorView.setOnClickListener(new View.OnClickListener() {
-	            
-	            @Override
-	            public void onClick(View v) {//Temporary so the app doesn't crash when partitions are clicked
-	            }
-	        });
-	        errorView.setTextSize((float) 14.0);
-	        errorView.setTypeface(Typeface.DEFAULT_BOLD);
-	        errorView.setText("Sorry, your device either does not have a camera or its camera is not compatible with this application.");
-	        setContentView(errorView);
-        }
     }
     
     @Override
     public void onPause(){
-    	mCamera.release();
+        mCamera.release();
     }
     
-    /*@Override
+    @Override
     public void onResume(){
-    	mCamera = getCameraInstance();
-    }*/
-
+        if (checkCameraHardware(this)){
+            setContentView(R.layout.activity_main);
+            
+            // Create an instance of Camera
+            mCamera = getCameraInstance();
+            if (mCamera == null){
+            	Toast.makeText(getApplicationContext(), "Camera Unavailable", Toast.LENGTH_SHORT).show();
+            	showErrorView();
+            	return;
+            }
+            Camera.Parameters cp = mCamera.getParameters();
+            List<Camera.Size> sizes = cp.getSupportedPreviewSizes();
+            cp.setPreviewSize(sizes.get(sizes.size()-1).width, sizes.get(sizes.size()-1).height);
+            mCamera.setParameters(cp);
+            
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview,0);
+        } else {
+            showErrorView();
+        }
+    }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -86,6 +69,26 @@ public class MainActivity extends Activity {
         } else {
             return false;
         }
+    }
+    
+    private void showErrorView(){
+    	TextView errorView;
+        errorView = new TextView(getApplicationContext());
+        errorView.setBackgroundResource(android.R.drawable.dark_header);
+        errorView.setGravity(Gravity.CENTER_VERTICAL);
+        errorView.setFocusable(false);
+        errorView.setClickable(false);
+        errorView.setLongClickable(false);
+        errorView.setOnClickListener(new View.OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        errorView.setTextSize((float) 14.0);
+        errorView.setTypeface(Typeface.DEFAULT_BOLD);
+        errorView.setText("Sorry, your device either does not have a camera or its camera is not compatible with this application.");
+        setContentView(errorView);
     }
     
     /** A safe way to get an instance of the Camera object. */
